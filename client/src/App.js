460b12556fe7b06242ff70b0b1f6fdc24be59cde
Matthew Vu <Matthew.Vu@sjsu.edu>
2021-01-08 14:32:54 -0800

    Implementation of example program with react and flask-socketio

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import RouterComp from './Router';
 
 function App() {
+  const dontShow = true;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World!
-        </a>
-      </header>
+      { dontShow ? <> </> :
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Hello World!
+          </a>
+        </header>
+      }
+      <RouterComp />
     </div>
   );
 }
