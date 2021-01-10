c83ce9b497a2cf25ba3ecb0460f5d2992828dc48
Matthew Vu <Matthew.Vu@sjsu.edu>
2021-01-10 02:10:28 -0800

    Added stack

--- client/src/GameRoom/GameRoom.jsx
+++ client/src/GameRoom/GameRoom.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Redirect} from "react-router-dom";
+import { Redirect } from "react-router-dom";
 
 import useGameState from "./GameState";
 import ChatRoom from "../ChatRoom/ChatRoom";
@@ -7,13 +7,13 @@ import PlayerField from "../Components/PlayerField";
 
 import "./GameRoom.css";
 
-const handleCardsPlayed = (field, stack, updateGameState) =>{
-  const currentCardSize = stack[stack.length-1];
+const handleCardsPlayed = (field, stack, updateGameState) => {
+  const currentCardSize = stack[stack.length - 1];
 
   // Instant win conditions
 
   // Standard checking
-  if(field.length !== currentCardSize){
+  if (field.length !== currentCardSize) {
     return;
   }
 
@@ -21,49 +21,112 @@ const handleCardsPlayed = (field, stack, updateGameState) =>{
 
   const gameAction = {
     newStack: field,
-  }
+  };
 
-  updateGameState(gameAction)
-}
+  updateGameState(gameAction);
+};
 
 const nullState = {
   roomId: "",
-  username: ""
-}
+  username: "",
+};
 
 const GameRoom = (props) => {
-  const { roomId, username } = props.location.state !== undefined ? props.location.state : nullState;
-  const [ hand, setHand ] = React.useState([]);
-  const [ field, setField ] = React.useState([]);
-  const [ stack, setStack ] = React.useState([[]]);
-  const [ newGameAction, setNewGameAction ] = React.useState(undefined);
-  const { gameState, updateGameState, messages, sendMessage, validPlayer, ready, readyPlayer } = useGameState(roomId, username, setHand);
+  const { roomId, username } =
+    props.location.state !== undefined ? props.location.state : nullState;
+  const [hand, setHand] = React.useState([]);
+  const [field, setField] = React.useState([]);
+  const [stack, setStack] = React.useState([[
+      {
+        filepath: "2D.jpg",
+        suit: "Diamonds",
+        value: 2,
+      },
+      {
+        filepath: "KH.jpg",
+        suit: "Hearts",
+        value: 13,
+      },
+      {
+        filepath: "JC.jpg",
+        suit: "Clubs",
+        value: 11,
+      },
+      {
+        filepath: "4S.jpg",
+        suit: "Spades",
+        value: 4,
+      },
+      {
+        filepath: "7D.jpg",
+        suit: "Diamonds",
+        value: 7,
+      },
+    ],
+  ]);
+  const [newGameAction, setNewGameAction] = React.useState(undefined);
+  const {
+    gameState,
+    updateGameState,
+    messages,
+    sendMessage,
+    validPlayer,
+    ready,
+    readyPlayer,
+  } = useGameState(roomId, username, setHand);
 
-  if(!validPlayer){
-   return(
-     <Redirect push to="/" />
-   )
+  if (!validPlayer) {
+    return <Redirect push to="/" />;
   }
 
   return (
     <div className="chat-room-container">
       <h1 className="room-name">Room: {roomId}</h1>
       <h3 className="room-name">Joined as: {username}</h3>
+
+      <div className="stack-container">
+        {stack.map((items, indx) =>
+          items.map((item, indx) => (
+            <img src={"/Images/" + item.filepath} alt={item.id} className="card" />
+          ))
+        )}
+      </div>
+
       <div>
-        <button className="button" disabled={ready} onClick={()=>{readyPlayer()}}>
+        <button
+          className="button"
+          disabled={ready}
+          onClick={() => {
+            readyPlayer();
+          }}
+        >
           Ready for Game
         </button>
 
-        <button className="button" disabled={field.length === 0} onClick={()=>{handleCardsPlayed(field, stack)}}> Confirm Selected Cards </button>
+        <button
+          className="button"
+          disabled={field.length === 0}
+          onClick={() => {
+            handleCardsPlayed(field, stack);
+          }}
+        >
+          {" "}
+          Confirm Selected Cards{" "}
+        </button>
       </div>
 
-      {stack.map((item, indx) =>(
-
-        
-      ))}
-
-      <PlayerField hand={hand} field={field} setHand={setHand} setField={setField} />
-      <ChatRoom roomId={roomId} messages={messages} sendMessage={sendMessage} updateGameState={updateGameState} />
+      <PlayerField
+        hand={hand}
+        field={field}
+        setHand={setHand}
+        setField={setField}
+      />
+      <ChatRoom
+        roomId={roomId}
+        messages={messages}
+        sendMessage={sendMessage}
+        updateGameState={updateGameState}
+      />
     </div>
   );
 };
